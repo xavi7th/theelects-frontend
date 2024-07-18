0a37895c69568c4933249518b5f2718b5398ba7c
xavi7th <xavi7th@gmail.com>
2024-07-18 01:18:36 +0100

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,29 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    alias: {
+      '$vendor': '/src/vendor',
+    },
+    // prerender: {
+    //   //@see https://kit.svelte.dev/docs/configuration#prerender
+    //   handleMissingId: ({ path, referrers, message, id }) => {
+
+    //     if (id === '/') return;
+
+    //     console.log({ path, referrers, message, id });
+
+		// 		throw new Error(message);
+		// 	},
+
+    //   //@see https://kit.svelte.dev/docs/configuration#prerender
+    //   handleHttpError: ({ referenceType, path, referrer, message, status }) => {
+
+    //     console.log({ referenceType, path, referrer, message, status});
+
+    //     throw new Error(message);
+		// 	}
+    // }
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import concat from 'rollup-plugin-concat';
 import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+    concat({
+      // groupedFiles: [
+      //   {
+      //     files: [
+      //       './src/vendor/js/jquery.js',
+      //       './src/vendor/js/bootstrap.min.js',
+      //       './src/vendor/js/owl.js',
+      //       './src/vendor/js/appear.js',
+      //       './src/vendor/js/wow.js',
+      //       './src/vendor/js/jquery.easing.min.js',
+      //     ],
+      //     outputFile: './static/js/app-vendor.js',
+      //   },
+      // ],
+    }),
+    enhancedImages(),
+    sveltekit()
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '$vendor/css/variables' as *;
+        `,
+      },
+    },
+  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
