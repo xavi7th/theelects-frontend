2084a9454c4f286aed14eb6328e65e4f15ad2202
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-09-13 13:36:59 +0100

    Update config for better cross-platform compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-    // => @media (min-width: Xpx) { ... }
-    screens: {
-      'xs': '320px',
-      'sm': '576px',
-      'md': '768px',
-      'lg': '992px',
-      'xl': '1200px',
-      'xxl': '1400px',
+  safelist: ["dark"],
+  darkMode: ["class"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      // => @media (min-width: Xpx) { ... }
+      screens: {
+        xs: '320px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1400px',
+      },
     },
-		extend: {}
-	},
-
-	plugins: [require('@tailwindcss/typography')]
+    extend: {}
+  },
+  plugins: [
+    require( '@tailwindcss/forms' ),
+    require( '@tailwindcss/typography' ),
+  ]
 };
